f38c5703c1c0a328a8611e368120f52bdc0b28c4
Darby Anderson <darby3232@gmail.com>
2023-05-11 20:24:15 -0500

    Updates to the array class.

--- src/base/array.hpp
+++ src/base/array.hpp
@@ -9,146 +9,91 @@
 
 namespace puffin {
 
-    template <typename T>
+    template<typename T>
     struct Array {
         Array();
+
         ~Array();
 
-        void                    init(Allocator*  allocator, u32 initial_capacity, u32 initial_size = 0);
-        void                    shutdown();
+        void init(Allocator* allocator, u32 initial_capacity, u32 initial_size = 0);
 
-        void                    push(const T& element);
-        T&                      push_use();                 // Grow the size and return T to be filled
+        void shutdown();
 
-        void                    pop();
-        void                    delete_swap(u32 index);
+        void push(const T& element);
 
-        T&                      operator[](u32 index);       // can change the value returned from the array
-        const T&                operator[](u32 index) const; // useful when we want to consider the array "read-only"
+        T& push_use();                 // Grow the size and return T to be filled
 
-        void                    clear();
-        void                    set_size(u32 new_size);
-        void                    set_capacity(u32 new_capacity); // WHATS THE DIFF?
-        void                    grow(u32 new_capacity);
+        void pop();
 
-        T&                      back();
-        const T&                back() const;
+        void delete_swap(u32 index);
 
-        T&                      front();
-        const T&                front() const;
+        T& operator[](u32 index);       // can change the value returned from the array
+        const T& operator[](u32 index) const; // useful when we want to consider the array "read-only"
 
-        u32                     size_in_bytes() const;
-        u32                     capacity_in_bytes() const;
+        void clear();
 
-        T*                      data;
-        u32                     size;       // Occupied size
-        u32                     capacity;   // Allocated capacity
-        Allocator*              allocator;
+        void set_size(u32 new_size);
+
+        void set_capacity(u32 new_capacity); // WHATS THE DIFF?
+        void grow(u32 new_capacity);
+
+        T& back();
+
+        const T& back() const;
+
+        T& front();
+
+        const T& front() const;
+
+        u32 size_in_bytes() const;
+
+        u32 capacity_in_bytes() const;
+
+        T* data;
+        u32 size;       // Occupied size
+        u32 capacity;   // Allocated capacity
+        Allocator* allocator;
     };
 
+    // View over a contiguous memory block
     template<typename T>
-    u32 Array<T>::capacity_in_bytes() const {
-        return capacity * sizeof(T);
-    }
+    struct ArrayView {
+        ArrayView(T* data, u32 size);
+
+        void set(T* data, u32 size);
+
+        T& operator[](u32 index);
+
+        const T& operator[](u32 index) const;
+
+        T* data;
+        u32 size;
+    };
+
+    // Array
+    // Array
 
     template<typename T>
-    u32 Array<T>::size_in_bytes() const {
-        return size * sizeof(T);
-    }
+    inline Array<T>::Array() {}
 
     template<typename T>
-    const T& Array<T>::front() const {
-        PASSERT(size);
-        return data[0];
-    }
+    inline Array<T>::~Array() {}
 
     template<typename T>
-    T& Array<T>::front() {
-        PASSERT(size);
-        return data[0];
-    }
+    inline void Array<T>::init(Allocator* allocator_, u32 initial_capacity, u32 initial_size) {
+        data = nullptr;
+        size = initial_size;
+        capacity = 0;
+        allocator = allocator_;
 
-    template<typename T>
-    const T& Array<T>::back() const {
-        PASSERT(size);
-        return data[size - 1];
-    }
-
-    template<typename T>
-    T& Array<T>::back() {
-        PASSERT(size);
-        return data[size - 1];
-    }
-
-    template<typename T>
-    void Array<T>::set_capacity(u32 new_capacity) {
-        if(new_capacity > capacity) {
-            grow(new_capacity);
+        if (initial_capacity > 0) {
+            grow(initial_capacity);
         }
     }
 
     template<typename T>
-    void Array<T>::set_size(u32 new_size) {
-        if(new_size > capacity) {
-            grow(new_size);
-        }
-        size = new_size;
-    }
-
-    template<typename T>
-    void Array<T>::clear() {
-        size = 0;
-    }
-
-    template<typename T>
-    const T& Array<T>::operator[](u32 index) const {
-        PASSERT(index < size);
-        return data[index];
-    }
-
-    template<typename T>
-    T& Array<T>::operator[](u32 index) {
-        PASSERT(index < size);
-        return data[index];
-    }
-
-    template<typename T>
-    void Array<T>::delete_swap(u32 index) {
-        PASSERT(size > 0);
-        size--;
-        data[index] = data [size];
-    }
-
-    template<typename T>
-    void Array<T>::pop() {
-        PASSERT(size > 0);
-        size--;
-    }
-
-    template<typename T>
-    T& Array<T>::push_use() {
-        if(size >= capacity) {
-            grow(capacity + 1);
-        }
-        size++;
-
-        return back();
-    }
-
-    template<typename T>
-    void Array<T>::push(const T& element) {
-        if(size >= capacity) {
-            grow(capacity + 1);
-        }
-
-        data[size++] = element;
-    }
-
-
-
-    template<typename T>
-    void Array<T>::shutdown() {
-        if(capacity > 0) {
+    inline void Array<T>::shutdown() {
+        if (capacity > 0) {
             allocator->deallocate(data);
         }
 
@@ -157,20 +102,144 @@ namespace puffin {
         capacity = 0;
     }
 
+    template<typename T>
+    inline void Array<T>::grow(u32 new_capacity) {
+        if (new_capacity < capacity * 2) {
+            new_capacity = capacity * 2;
+        } else if (new_capacity < 4) {
+            new_capacity = 4;
+        }
 
+        T* new_data = (T*) allocator->allocate(new_capacity * sizeof(T), alignof(T));
+        if (capacity) {
+            memory_copy(new_data, data, capacity * sizeof(T));
+            allocator->deallocate(data);
+        }
 
+        data = new_data;
+        capacity = new_capacity;
+    }
 
-    // View over a contiguous memory block
-    template <typename T>
-    struct ArrayView {
-        ArrayView(T* data, u32 size);
+    template<typename T>
+    inline u32 Array<T>::capacity_in_bytes() const {
+        return capacity * sizeof(T);
+    }
 
-        void                set(T* data, u32 size);
+    template<typename T>
+    inline u32 Array<T>::size_in_bytes() const {
+        return size * sizeof(T);
+    }
 
-        T&                  operator[](u32 index);
-        const T&            operator[](u32 index) const;
+    template<typename T>
+    inline const T& Array<T>::front() const {
+        PASSERT(size);
+        return data[0];
+    }
 
-        T*                  data;
-        u32                 size;
-    };
+    template<typename T>
+    inline T& Array<T>::front() {
+        PASSERT(size);
+        return data[0];
+    }
+
+    template<typename T>
+    inline const T& Array<T>::back() const {
+        PASSERT(size);
+        return data[size - 1];
+    }
+
+    template<typename T>
+    inline T& Array<T>::back() {
+        PASSERT(size);
+        return data[size - 1];
+    }
+
+    template<typename T>
+    inline void Array<T>::set_capacity(u32 new_capacity) {
+        if (new_capacity > capacity) {
+            grow(new_capacity);
+        }
+    }
+
+    template<typename T>
+    inline void Array<T>::set_size(u32 new_size) {
+        if (new_size > capacity) {
+            grow(new_size);
+        }
+        size = new_size;
+    }
+
+    template<typename T>
+    inline void Array<T>::clear() {
+        size = 0;
+    }
+
+    template<typename T>
+    inline const T& Array<T>::operator[](u32 index) const {
+        PASSERT(index < size);
+        return data[index];
+    }
+
+    template<typename T>
+    inline T& Array<T>::operator[](u32 index) {
+        PASSERT(index < size);
+        return data[index];
+    }
+
+    template<typename T>
+    inline void Array<T>::delete_swap(u32 index) {
+        PASSERT(size > 0);
+        size--;
+        data[index] = data[size];
+    }
+
+    template<typename T>
+    inline void Array<T>::pop() {
+        PASSERT(size > 0);
+        size--;
+    }
+
+    template<typename T>
+    inline T& Array<T>::push_use() {
+        if (size >= capacity) {
+            grow(capacity + 1);
+        }
+        size++;
+
+        return back();
+    }
+
+    template<typename T>
+    inline void Array<T>::push(const T& element) {
+        if (size >= capacity) {
+            grow(capacity + 1);
+        }
+
+        data[size++] = element;
+    }
+
+    // ArrayView //////////////////////////////////////////////
+
+    template<typename T>
+    inline ArrayView<T>::ArrayView(T* data_, u32 size_)
+        : data(data_), size(size_) {
+    }
+
+    template<typename T>
+    inline const T& ArrayView<T>::operator[](u32 index) const {
+        PASSERT(index  < size);
+        return data[index];
+    }
+
+    template<typename T>
+    inline T& ArrayView<T>::operator[](u32 index) {
+        PASSERT(index < size);
+        return data[index];
+    }
+
+    template<typename T>
+    inline void ArrayView<T>::set(T* data_, u32 size_) {
+        data = data_;
+        size = size_;
+    }
 }
