b3d3266c5c471b1707ea3071ae4bd350b02e3297
Darby Anderson <darby3232@gmail.com>
2023-05-12 16:04:54 -0500

    Update to the window class.

--- src/application/window.hpp
+++ src/application/window.hpp
@@ -5,4 +5,37 @@
 #pragma once
 
 #include "service.hpp"
-#include  "array.hpp"
+#include "array.hpp"
+
+namespace puffin {
+
+    struct WindowConfiguration {
+        u32             width;
+        u32             height;
+
+        cstring         name;
+        Allocator*      allocator;
+    };
+
+    typedef void        (*OsMessagesCallback) (void* os_event, void* user_data);
+
+    struct Window: public Service {
+        void            init(void* configuration) override;
+        void            shutdown() override;
+
+        void            handle_os_messages();
+
+        void            set_fullscreen(bool value);
+
+        void            register_os_messages_callback(OsMessagesCallback callback, void* user_data);
+        void            deregister_os_messages_callback(OsMessagesCallback callback);
+
+        void            center_mouse(bool dragging);
+
+          
+
+
+    };
+
+
+}
