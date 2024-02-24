bb5517fd5b7ecf0cb4ab69517bd86d312f5c5718
Darby <darby3232@gmail.com>
2024-02-23 18:51:12 -0600

    Update

--- src/graphics/spirv_parser.cpp
+++ src/graphics/spirv_parser.cpp
@@ -96,8 +96,210 @@ void parse_binary(const u32* data, size_t data_size, StringBuffer& name_buffer, 
         switch (op) {
             case (SpvOpEntryPoint):
             {
-                //STARTHERE
+                PASSERT(word_count >= 4);
+
+                SpvExecutionModel model = (SpvExecutionModel)data[word_index + 1];
+                stage = parse_execution_model(model);
+                PASSERT(stage != 0);
+
+                break;
             }
+            case (SpvOpDecorate):
+            {
+                PASSERT(word_count >= 3);
+
+                u32 id_index = data[word_index + 1];
+                PASSERT(id_index < id_bound);
+
+                Id& id = ids[id_index];
+
+                SpvDecoration decoration = (SpvDecoration)data[word_count + 2];
+                switch(decoration) {
+                    case(SpvDecorationBinding):
+                    {
+                        id.binding = data[word_index + 3];
+                        break;
+                    }
+                    case(SpvDecorationDescriptorSet):
+                    {
+                        id.binding = data[word_index + 3];
+                        break;
+                    }
+                }
+
+                break;
+            }
+            case (SpvOpMemberDecorate):
+            {
+                PASSERT(word_count >= 4);
+
+                u32 id_index = data[word_index + 1];
+                PASSERT(id_index < id_bound);
+
+                Id& id = ids[id_index];
+
+                u32 member_index = data[word_index + 2];
+
+                if(id.members.capacity == 0) {
+                    id.members.init(allocator, 64, 64);
+                }
+
+                Member& member = id.members[member_index];
+
+                SpvDecoration decoration = (SpvDecoration)data[word_index + 3];
+                switch(decoration)
+                {
+                    case(SpvDecorationOffset):
+                    {
+                        member.offset = data[word_index + 4];
+                        break;
+                    }
+                }
+
+                break;
+            }
+            case(SpvOpName):
+            {
+                PASSERT(word_count >= 3);
+
+                u32 id_index = data[word_index + 1];
+                PASSERT(id_index < id_bound);
+
+                Id& id = ids[id_index];
+
+                char* name = (char*)(data + (word_index + 2));
+                char* name_view = name_buffer.append_use(name);
+
+                id.name.text = name_view;
+                id.name.length = strlen(name_view);
+
+                break;
+            }
+            case(SpvOpMemberName):
+            {
+                PASSERT(word_count >= 4);
+
+                u32 id_index = data[word_index + 1];
+                PASSERT(id_index < id_bound);
+
+                Id& id = ids[id_index];
+
+                u32 member_index = data[word_index + 2];
+
+                if(id.members.capacity == 0) {
+                    id.members.init(allocator, 64, 64);
+                }
+
+                Member& member = id.members[member_index];
+
+                char* name = (char*)(data + (word_index + 3));
+                char* name_view = name_buffer.append_use(name);
+
+                member.name.text = name_view;
+                member.name.length = strlen(name_view);
+
+                break;
+            }
+            case(SpvOpTypeInt):
+            {
+                PASSERT(word_count == 4);
+
+                u32 id_index = data[word_index + 1];
+                PASSERT(id_index < id_bound);
+
+                Id& id = ids[id_index];
+                id.op = op;
+                id.width = (u8) data[word_index + 2];
+                id.sign = (u8) data[word_index + 3];
+
+                return;
+            }
+            case ( SpvOpTypeFloat ):
+            {
+                PASSERT( word_count == 3 );
+
+                u32 id_index = data[ word_index + 1 ];
+                PASSERT( id_index < id_bound );
+
+                Id& id= ids[ id_index ];
+                id.op = op;
+                id.width = ( u8 )data[ word_index + 2 ];
+
+                break;
+            }
+            case ( SpvOpTypeVector ):
+            {
+                PASSERT( word_count == 4 );
+
+                u32 id_index = data[ word_index + 1 ];
+                PASSERT( id_index < id_bound );
+
+                Id& id= ids[ id_index ];
+                id.op = op;
+                id.type_index = data[ word_index + 2 ];
+                id.count = data[ word_index + 3 ];
+
+                break;
+            }
+            case ( SpvOpTypeMatrix ):
+            {
+                PASSERT( word_count == 4 );
+
+                u32 id_index = data[ word_index + 1 ];
+                PASSERT( id_index < id_bound );
+
+                Id& id= ids[ id_index ];
+                id.op = op;
+                id.type_index = data[ word_index + 2 ];
+                id.count = data[ word_index + 3 ];
+
+                break;
+            }
+            case ( SpvOpTypeImage ):
+            {
+                PASSERT( word_count >= 9 );
+
+                break;
+            }
+            case ( SpvOpTypeSampler ):
+            {
+                PASSERT( word_count == 2 );
+
+                u32 id_index = data[ word_index + 1 ];
+                PASSERT( id_index < id_bound );
+
+                Id& id= ids[ id_index ];
+                id.op = op;
+
+                break;
+            }
+            case ( SpvOpTypeSampledImage ):
+            {
+                PASSERT( word_count == 3 );
+
+                u32 id_index = data[ word_index + 1 ];
+                PASSERT( id_index < id_bound );
+
+                Id& id= ids[ id_index ];
+                id.op = op;
+
+                break;
+            }
+            case ( SpvOpTypeArray ):
+            {
+                PASSERT( word_count == 4 );
+
+                u32 id_index = data[ word_index + 1 ];
+                PASSERT( id_index < id_bound );
+
+                Id& id= ids[ id_index ];
+                id.op = op;
+                id.type_index = data[ word_index + 2 ];
+                id.count = data[ word_index + 3 ];
+
+                break;
+            }
+
         }
 
     }
